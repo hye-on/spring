bd24383b73651d4c4b4038f9d12f077ad9a9d5f9
hye-on <ain0103@naver.com>
2022-06-02 14:54:23 +0900

    스프링

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,9 +1,15 @@
 package hello.hellospring.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Member {
 
     //요구사항
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)//db가 알아서 생성->IDENTITY
     private Long Id;//고객이 저장 X 시스템이 구분하기 위해
+
+
     private String name;
 
     public Long getId() {
