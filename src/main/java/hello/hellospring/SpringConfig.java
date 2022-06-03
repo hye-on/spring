93b4d5bca10990bec6a619bafcab3d13bae6dc2c
hye-on <ain0103@naver.com>
2022-06-03 19:34:58 +0900

    스프링

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -12,25 +12,32 @@ import javax.sql.DataSource;
 public class SpringConfig {
 
 
-    private EntityManager em;
+   /* private EntityManager em;
 
     @Autowired
     public SpringConfig(EntityManager em) {
         this.em = em;
     }
+*/
+    private  final  MemberRepository memberRepository;
 
-    @Bean
-    public MemberService memberService(){
-        return new MemberService(memberRepository());
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository);
+    }
+
+  /*  @Bean
     public MemberRepository memberRepository(){
        // return new MemoryMemberRepository();
 
       //  return new JdbcMemberRepository(dataSource);
     //    return  new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+       // return new JpaMemberRepository(em);
+    }*/
 
 }
